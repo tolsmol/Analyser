323ae9d14517a347cfe27f4524b3d1b7db1d601c
Emily <e.f.powell@lancaster.ac.uk>
2020-04-15 13:44:25 +0100

    created Task properties, created Context file

--- Task.java
+++ Task.java
@@ -1,1 +1,35 @@
 package emyflorence.Analyser;
+import java.util.ArrayList; // import the ArrayList class
+
+public class Task {
+    int id;
+    String name;
+    int projectID;
+    enum Type {
+        PROJECT,
+        TASK,
+        PLAN,
+        CONTACT
+    }
+    boolean inbox;
+    LocalDateTime dueDate;
+    ArrayList<Context> contexts;
+    ArrayList<Task> dependsOn;
+    ArrayList<Task> depending;
+    double totalPriority;
+    int priority;
+    ArrayList<Task> subTasks;
+    String path;
+    LocalDateTime dateAdded;
+    int order;
+    enum Status {
+        INCOMPLETE,
+        WAITING,
+        THINKING,
+        IN PROGRESS,
+        NEARLY FINISHED,
+        DONE,
+        SOMEDAY
+    }
+
+}
